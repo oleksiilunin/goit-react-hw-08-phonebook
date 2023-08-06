8f3fb2acb2746468e5bd7a962199bab11dfb1abc
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-08-06 18:35:26 +0000

    Deploying to gh-pages from @ oleksiilunin/goit-react-hw-08-phonebook@8a50ea0a10c775d71cc9fec2cf972b8c9a9b6c60 🚀

---
+++ static/js/859.654f6623.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[859],{4859:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184);function c(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"THIS IS YOUR PHONEBOOK"})})}}}]);
+//# sourceMappingURL=859.654f6623.chunk.js.map
